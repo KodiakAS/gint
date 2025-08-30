a8e8ada93d282deed6e3bc467a63c17ee9ddd6b1
KodiakAS <38487204+KodiakAS@users.noreply.github.com>
2025-08-30 11:41:55 +0800

    test: cover additional numeric_limits functions

--- tests/gint_test.cpp
+++ tests/gint_test.cpp
@@ -848,6 +848,22 @@ TEST(WideIntegerNumericLimits, Basic)
     expected <<= 127;
     expected = -expected;
     EXPECT_EQ(smin, expected);
+
+    EXPECT_EQ(std::numeric_limits<U>::lowest(), std::numeric_limits<U>::min());
+    EXPECT_EQ(std::numeric_limits<U>::epsilon(), U(0));
+    EXPECT_EQ(std::numeric_limits<U>::round_error(), U(0));
+    EXPECT_EQ(std::numeric_limits<U>::infinity(), U(0));
+    EXPECT_EQ(std::numeric_limits<U>::quiet_NaN(), U(0));
+    EXPECT_EQ(std::numeric_limits<U>::signaling_NaN(), U(0));
+    EXPECT_EQ(std::numeric_limits<U>::denorm_min(), U(0));
+
+    EXPECT_EQ(std::numeric_limits<S>::lowest(), std::numeric_limits<S>::min());
+    EXPECT_EQ(std::numeric_limits<S>::epsilon(), S(0));
+    EXPECT_EQ(std::numeric_limits<S>::round_error(), S(0));
+    EXPECT_EQ(std::numeric_limits<S>::infinity(), S(0));
+    EXPECT_EQ(std::numeric_limits<S>::quiet_NaN(), S(0));
+    EXPECT_EQ(std::numeric_limits<S>::signaling_NaN(), S(0));
+    EXPECT_EQ(std::numeric_limits<S>::denorm_min(), S(0));
 }
 
 TEST(WideIntegerBuiltin, FloatingTypes)
